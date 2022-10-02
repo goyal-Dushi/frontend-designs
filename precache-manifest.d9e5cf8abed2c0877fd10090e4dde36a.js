b43c33dc078dbedd76fa8831f39c0abda22bf786
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-10-02 13:21:33 +0530

    Updates

---
+++ precache-manifest.d9e5cf8abed2c0877fd10090e4dde36a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "834feab064c7658eb4cc",
+    "url": "/frontend-designs/static/css/main.661e30e1.chunk.css"
+  },
+  {
+    "revision": "834feab064c7658eb4cc",
+    "url": "/frontend-designs/static/js/main.3850e0e7.chunk.js"
+  },
+  {
+    "revision": "a04d84944b500222d259",
+    "url": "/frontend-designs/static/js/runtime~main.7a752ba6.js"
+  },
+  {
+    "revision": "7ff16004bef0758fa6a0",
+    "url": "/frontend-designs/static/css/2.6060c30a.chunk.css"
+  },
+  {
+    "revision": "7ff16004bef0758fa6a0",
+    "url": "/frontend-designs/static/js/2.17d26bb2.chunk.js"
+  },
+  {
+    "revision": "645e63fd7825180567b5196e9e2de599",
+    "url": "/frontend-designs/static/media/stats-card-preview.645e63fd.jpg"
+  },
+  {
+    "revision": "37a5b4c36d252e64c2b0260239841351",
+    "url": "/frontend-designs/static/media/image-header-desktop.37a5b4c3.jpg"
+  },
+  {
+    "revision": "9e7058d90b7a763afbfaaaeb6f893411",
+    "url": "/frontend-designs/index.html"
+  }
+];
