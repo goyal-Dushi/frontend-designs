b81216b9cbd9e1e8d8ccb14fa1bf99557d9bc4a9
goyal-Dushi <dushyantgoyal28@gmail.com>
2024-03-25 18:53:20 +0530

    update route for countries api app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useMemo, useReducer } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { data } from './cardsData';
 import Card from './components/common/Card';
 import Meta, { MetaProps } from './components/common/Meta';
 import AdviceGenerator from './pages/AdviceGenerator';
+import CountriesSwitcher, {
+  CountriesSwitcherThemeContext,
+} from './pages/CountriesSwitcher';
+import CountryDetails from './pages/CountriesSwitcher/components/CountryDetails';
+import themeReducer from './pages/CountriesSwitcher/reducer/themeReducer';
 import JobListing from './pages/JobListing';
 import RPSpage from './pages/RPSgame';
 import StatsPreviewCard from './pages/StatsPreviewCard';
@@ -45,15 +50,36 @@ const App: React.FC<AppProps> = () => (
 
 interface MainProps {}
 
-export const Main: React.FC<MainProps> = () => (
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/statsCardPreview" element={<StatsPreviewCard />} />
-      <Route path="/adviceGenerator" element={<AdviceGenerator />} />
-      <Route path="/jobListing" element={<JobListing />} />
-      <Route path="/rpsGamePage" element={<RPSpage />} />
-      <Route path="/tipGenerator" element={<TipGenerator />} />
-    </Routes>
-  </HashRouter>
-);
+export const Main: React.FC<MainProps> = () => {
+  const [theme, dispatchTheme] = useReducer(themeReducer, 'light');
+
+  const contextValue = useMemo(
+    () => ({ theme, dispatchTheme }),
+    [theme, dispatchTheme]
+  );
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/statsCardPreview" element={<StatsPreviewCard />} />
+        <Route path="/adviceGenerator" element={<AdviceGenerator />} />
+        <Route path="/jobListing" element={<JobListing />} />
+        <Route path="/rpsGamePage" element={<RPSpage />} />
+        <Route path="/tipGenerator" element={<TipGenerator />} />
+        <Route
+          path="/countries/*"
+          element={
+            // eslint-disable-next-line react/jsx-wrap-multilines
+            <CountriesSwitcherThemeContext.Provider value={contextValue}>
+              <Routes>
+                <Route path="/" element={<CountriesSwitcher />} />
+                <Route path=":country" element={<CountryDetails />} />
+              </Routes>
+            </CountriesSwitcherThemeContext.Provider>
+          }
+        />
+      </Routes>
+    </HashRouter>
+  );
+};
